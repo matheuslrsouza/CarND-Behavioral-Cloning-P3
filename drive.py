36d47a9b177a24cdf6a8ed6309d438015421216f
Matheus Souza <matheuslrsouza@gmail.com>
2017-08-14 11:22:46 -0300

    change input to pass Y image

--- drive.py
+++ drive.py
@@ -16,6 +16,8 @@ from keras.models import load_model
 import h5py
 from keras import __version__ as keras_version
 
+import cv2
+
 sio = socketio.Server()
 app = Flask(__name__)
 model = None
@@ -36,7 +38,7 @@ class SimplePIController:
     def update(self, measurement):
         # proportional error
         self.error = self.set_point - measurement
-
+        
         # integral error
         self.integral += self.error
 
@@ -61,7 +63,17 @@ def telemetry(sid, data):
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
-        steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
+
+        #gray = cv2.cvtColor(image_array, cv2.COLOR_BGR2GRAY)
+        #gray = gray.reshape(160, 320, -1)
+        
+        
+        yuv = cv2.cvtColor(image_array, cv2.COLOR_BGR2YUV)
+        y, u, v = cv2.split(yuv)
+        
+        y = y.reshape(160, 320, -1)
+        
+        steering_angle = float(model.predict(y[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
 
