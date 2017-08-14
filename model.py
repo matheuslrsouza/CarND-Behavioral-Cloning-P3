a41744760dc8d22956fc1d5752a018a96576cd5b
Matheus Souza <matheuslrsouza@gmail.com>
2017-08-14 11:23:34 -0300

    add comments

--- model.py
+++ model.py
@@ -3,9 +3,13 @@ import cv2
 import numpy as np
 from sklearn.utils import shuffle
 import pickle
+from sklearn.model_selection import train_test_split
 
+# all paths for the sample images
 samples = []
 
+# given a dir path, reads the driving_log.csv to extract the path for images.
+# replaces the path images and adds the line to the "samples" vector
 def load_data(dir_data):
 	with open(dir_data + 'driving_log.csv') as csvfile:
 		reader = csv.reader(csvfile)
@@ -18,52 +22,26 @@ def load_data(dir_data):
 
 			samples.append(line)
 
-load_data('../track1/') # track 1
-load_data('../track1_1/')
-load_data('../track1_2/')
-load_data('../track1_3/')
-load_data('../track1_4/')
-load_data('../track1_5/')
-load_data('../track1_6/')
-load_data('../track2/')
-load_data('../track2_1/')
+# loads the image paths from these directories
+# this was usefull because the transfer to the ec2 amazon cloud could 
+# did on demand (as needed)
+load_data('../../track1/') 
+load_data('../../track1_1/')
+load_data('../../track1_2/')
+load_data('../../track1_3/')
+load_data('../../track1_4/')
+load_data('../../track1_5/')
+load_data('../../track1_6/')
+load_data('../../track2/')
+load_data('../../track2_1/')
 
+# print the number of samples
 print("samples ", len(samples))
 
-
-def get_data(samples):
-
-	images = []	
-	measurements = []
-
-	for line in samples:
-
-		correction = 0.2
-
-		steering_center = float(line[3]) #steering
-		steering_left = steering_center + correction
-		steering_right = steering_center - correction
-
-		image_center = cv2.imread(line[0])
-		image_left = cv2.imread(line[1])
-		image_right = cv2.imread(line[2])	
-		
-		images.extend([image_left])
-		images.extend([image_center])
-		images.extend([image_right])
-
-		measurements.extend([steering_left])
-		measurements.extend([steering_center])
-		measurements.extend([steering_right])
-
-	X_train = np.array(images)
-	y_train = np.array(measurements)
-	return shuffle(X_train, y_train)
-
-
-from sklearn.model_selection import train_test_split
+# 20% for validation data
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
+# generator to provide the data for training
 def get_data_generator(samples, batch_size=32):
 	num_samples = len(samples)
 	while 1: # Loop forever so the generator never terminates
@@ -77,13 +55,18 @@ def get_data_generator(samples, batch_size=32):
 
 				correction = 0.2
 
-				steering_center = float(line[3]) #steering
+				#steering
+				steering_center = float(line[3]) 
+				# adds a correction for the images captured on the left and right cameras
+				# this allows the car to be redirected to the center if it is leaving the runway
 				steering_left = steering_center + correction
 				steering_right = steering_center - correction
 
+				# reads the paths for the three captured images
 				image_left = cv2.imread(line[1])
 				image_center = cv2.imread(line[0])
 				image_right = cv2.imread(line[2])
+
 
 				split_yuv_and_add(images, angles, image_left, steering_left)
 				split_yuv_and_add(images, angles, image_center, steering_center)
@@ -94,13 +77,17 @@ def get_data_generator(samples, batch_size=32):
 			
 			yield shuffle(X_train, y_train)
 
+# splits the given image in YUV to produce augmentation data
 def split_yuv_and_add(images, angles, img, steering):
 	yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
 	y, u, v = cv2.split(yuv)
+
+	# before add the, reshapes the images to one channel on last dimenstion (160, 320, 1)
 	images.append(y.reshape(160, 320, -1))
 	images.append(u.reshape(160, 320, -1))
 	images.append(v.reshape(160, 320, -1))
 
+	# repeats the same steering angles three times (Y, U and V)
 	angles.append(steering)
 	angles.append(steering)
 	angles.append(steering)
@@ -122,7 +109,7 @@ history_object = network.train_generator(train_generator, validation_generator,
 									len(validation_samples)/batch_size)
 
 ### save the history so we can view it at any time. 
-### when we are training on GPU we can't plot 
+### when we are training on GPU we can't plot there
 pickle.dump(history_object.history, open('loss_history.p', 'wb'))
 
 
--- netnvidia.py
+++ netnvidia.py
@@ -2,6 +2,7 @@ import keras
 from keras.models import Sequential
 from keras.layers import Dense, Flatten, Lambda, Dropout
 from keras.layers import Conv2D, MaxPooling2D, Cropping2D
+from keras.layers import AveragePooling2D
 from keras import optimizers
 
 class NVidia:
@@ -9,23 +10,23 @@ class NVidia:
 	def __init__(self):
 		self.model = Sequential()
 
-		self.model.add(Lambda(lambda x: (x / 255.0) -0.5, input_shape=(160, 320, 3)))
+		
+		self.model.add(Lambda(lambda x: (x / 255.0) -0.5, input_shape=(160, 320, 1)))
 		self.model.add(Cropping2D(cropping=((70, 20), (0, 0))))
 
 		self.model.add(Conv2D(24, (5, 5), strides=(2, 2), activation='relu'))
 		self.model.add(Conv2D(36, (5, 5), strides=(2, 2), activation='relu'))
 		self.model.add(Conv2D(48, (5, 5), strides=(2, 2), activation='relu'))
-		#self.model.add(Dropout(0.8))
 
 		self.model.add(Conv2D(64, (3, 3), strides=(1, 1), activation='relu'))
 		self.model.add(Conv2D(64, (3, 3), strides=(1, 1), activation='relu'))
 		
 		self.model.add(Flatten())
-		self.model.add(Dropout(0.6))
+		self.model.add(Dropout(0.4))
 		self.model.add(Dense(100, activation='relu'))
 		self.model.add(Dense(50, activation='relu'))
 		self.model.add(Dense(10, activation='relu'))
-		#self.model.add(Dropout(0.8))
+		
 		#output
 		self.model.add(Dense(1))
 
@@ -34,8 +35,6 @@ class NVidia:
 	def train_generator(self, train_generator, validation_generator, 
 				steps_per_epoch, validation_steps):
 
-		#adam = keras.optimizers.Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
-		#adam = keras.optimizers.Adagrad(lr=0.001, epsilon=1e-08, decay=0.0)
 		self.model.compile(loss='mse', optimizer='adam')
 
 		history = self.model.fit_generator(
@@ -47,10 +46,4 @@ class NVidia:
 		self.model.save('model.h5')
 
 		return history
-
-	def train(self, X_train, y_train):
-
-		self.model.compile(loss='mse', optimizer='adam')
-		self.model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=4)
-
-		self.model.save('model.h5')
+	
